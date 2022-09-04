89e043b997c4799af138e86737a07d2766e74ea3
GSQLin <2768244699@qq.com>
2022-09-04 14:05:51 +0800

    Initial commit

---
+++ src/main/java/me/gsqlin/ForgePlugin.java
@@ -0,0 +1,10 @@
+package me.gsqlin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ForgePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("§aPluginLoaded\n§aPluginLoaded\n§aPluginLoaded\n");
+    }
+}
